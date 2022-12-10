2beeb5d9ecefb3b1745032570b16e13d7cff940c
Sharkzuria <baumabulondo@gmail.com>
2022-12-10 22:14:18 +0300

    adding calories

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
 func main() {
-	panic("Not working!")
+	f, err := os.Open("calories.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// elves := map[int]int{}
+	reader := bufio.NewReader(f)
+	i := 0 
+
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, ok := 
+		if len(line) == 0 {
+
+		}
+		_, ok := elves[i]
+		if !ok {
+			elves[i] = calories
+		}
+		elves[i] += calories
+		calories, _ := strconv.Atoi(string(line))
+
+		fmt.Println(calories)
+
+	}
 }
